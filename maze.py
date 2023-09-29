28885b5234c03140fff6a88ea3df513a67a0e034
OJddJO <95376647+OJddJO@users.noreply.github.com>
2023-09-30 00:32:07 +0200

    a

--- maze.py
+++ maze.py
@@ -29,7 +29,7 @@ class Maze:
         for i, list in enumerate(self.maze):
             for j, value in enumerate(list):
                 if value == "u":
-                    unvisited.append([i, j])
+                    unvisited.append([i, j]) # [row, column]
         if len(unvisited) == 0:
             return
         if next == None:
@@ -38,27 +38,35 @@ class Maze:
             current = next
         self.maze[current[0]][current[1]] = "."
         direction = [(0, 2), (0, -2), (2, 0), (-2, 0)]
+        print(direction)
         #remove the direction if it is out of bound
+        print("out of bound")
         for dir in direction:
-            if current[0] + dir[0] <= 0 or current[0] + dir[0] >= self.height-1:
+            if current[0] + dir[0] < 1 or current[0] + dir[0] >= self.height-1:
+                print(0, dir[0], self.height-1)
                 direction.remove(dir)
-            elif current[1] + dir[1] <= 0 or current[1] + dir[1] >= self.width-1:
+            elif current[1] + dir[1] < 1 or current[1] + dir[1] >= self.width-1:
+                print(1, dir[1], self.width-1)
                 direction.remove(dir)
+            print(direction)
         #remove the direction if there is no wall to break or if next is path
+        print("no wall")
         for dir in direction:
-            if self.maze[current[0] + dir[0]//2][current[1] + dir[1]//2] == "." or self.maze[current[0] + dir[0]][current[1] + dir[1]] == ".":
+            if (self.maze[current[0] + dir[0]//2][current[1] + dir[0]//2] != "#" or self.maze[current[0] + dir[0]][current[1] + dir[1]] != "u"):
+                print(self.maze[current[0] + dir[0]//2][current[1] + dir[0]//2], self.maze[current[0] + dir[0]][current[1] + dir[1]])
                 direction.remove(dir)
+            print(direction)
         if len(direction) != 0:
-            random.shuffle(direction)
             randomDirection = random.choice(direction)
-            direction.remove(randomDirection)
-            self.maze[current[0] + randomDirection[0] // 2][current[1] + randomDirection[1] // 2] = "."
-            print(randomDirection, direction, current, current[0] + randomDirection[0], current[1] + randomDirection[1])
+            print(randomDirection, direction, current)
+            self.maze[current[0] + randomDirection[0]//2][current[1] + randomDirection[1]//2] = "."
+            current[0] += randomDirection[0]
+            current[1] += randomDirection[1]
         else:
             return
         self.print_maze()
         if len(unvisited) != 0:
-            self.generate_maze(next=[current[0] + randomDirection[0], current[1] + randomDirection[1]])
+            self.generate_maze(current)
 
     def print_maze(self):
         for i in range(self.height+1):
