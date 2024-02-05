3a270dcb8135c5c7d94222158f4db58d630170aa
OJddJO <95376647+OJddJO@users.noreply.github.com>
2024-02-05 18:19:19 +0100

    Bug fix and recompile

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from keyboard import press
+from keyboard import press, wait
 from time import sleep
 press('f11') #fullscreen
 sleep(0.5)
 print("Press 'f11' if not in fullscreen")
 print("Press 'Enter' to continue...")
+wait('enter')
 
 from classes.Game import MainMenu
 if __name__ == "__main__": #execute the game
